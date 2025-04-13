9404801170e00b42c7b52d008a68fdde9ea0daf5
stonekiln <stonepiece@outlook.jp>
2025-04-14 02:13:34 +0900

    頭働いてない

--- procon36_server/process/functions/orbitalSystem.js
+++ procon36_server/process/functions/orbitalSystem.js
@@ -3,11 +3,8 @@ const ReceiveData = require("./proconUtility").ReceiveData;
 
 class Lampyrisma extends Procon {
     garden;
-
-    position;
-
+    entities;
     width;
-
     depth;
 
     constructor(element, width, depth) {
@@ -24,48 +21,7 @@ class Lampyrisma extends Procon {
         }));
         this.width = width;
         this.depth = depth;
-        this.position = [...Array(this.size * this.size)];
-        for (let i = 0; i < this.size; i++) {
-            for (let j = 0; j < this.size; j++) {
-                this.position[this.board[i][j]] = [j, i];
-            }
-        }
-    }
-
-    calcCenter(value) {
-        let vector = [this.position[value % 2 == 0 ? value + 1 : value - 1][0] - this.position[value][0], -this.position[value % 2 == 0 ? value + 1 : value - 1][1] + this.position[value][1]];
-        if (Math.abs(vector[0]) > 1 || Math.abs(vector[1]) > 1 || (Math.abs(vector[0]) == 1 && Math.abs(vector[1]) == 1)) {
-            let center=this.position[value];
-            let direction = (vector[0] > 0 ? 2 : (vector[0] != 0) ? 5 : 1) * (vector[1] > 0 ? 7 : (vector[1] != 0) ? 3 : 1);
-            console.log(vector);
-            console.log(direction);
-            switch (direction) {
-                case 15:
-                    center[0] += 1;
-                case 35:
-                    center += [0,0];
-                case 14:
-                    center += [0, 0];
-            }
-            if (direction % 2 == 0) {
-                center[0] += this.position[value][0];
-                center[1] += this.position[value][1];
-            }
-            if (direction % 3 == 0) {
-                center[0] += this.position[value][0] + vector[1];
-                center[1] += this.position[value][1];
-            }
-            if (direction % 5 == 0) {
-                center[0] += this.position[value][0] + vector[0];
-                center[1] += this.position[value][1] - vector[0];
-            }
-            if (direction % 7 == 0) {
-                center[0] += this.position[value][0]
-                center[1] += this.position[value][1] + vector[1];
-            }
-            return { center: center, size: Math.abs(vector[0]) + Math.abs(vector[1]) };
-        }
-        return false;
+        this.entities = new EntityInfo(this.board);
     }
 
     engage(board, position, size, reverse = false) {
@@ -94,18 +50,84 @@ class Lampyrisma extends Procon {
             for (let j = 0; j < size; j++) {
                 if (reverse) {
                     board[i + position[1]][j + position[0]] = area[j][i];
-                    this.position[area[j][i]] = [j, i];
+                    this.entities.position[area[j][i]] = [i, j];
+                    this.entities.update(area[j][i]);
                 }
                 else {
                     board[i + position[1]][j + position[0]] = area[i][j];
-                    this.position[area[i][j]] = [i, j];
+                    this.entities.position[area[i][j]] = [j, i];
+                    this.entities.update(area[i][j]);
                 }
             }
         }
     }
 
     decodeBoard() {
-        this.board = this.board.map(array => array.map(element => Math.floor(element / 2)));
+        this.board = this.board.map(array => array.map(element => element != 0 ? Math.floor(element / 2) : 0));
+    }
+}
+
+class EntityInfo {
+    position;
+    vector;
+    direction;
+    distance;
+
+    constructor(board) {
+        let size = board.length;
+        this.position = new Array(size * size);
+        this.vector = new Array(size * size);
+        this.direction = new Array(size * size);
+        this.distance = new Array(size * size);
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                this.position[board[i][j]] = [j, i];
+            }
+        }
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                if (board[i][j] % 2 == 0) {
+                    this.update(board[i][j]);
+                }
+            }
+        }
+    }
+
+    update(value) {
+        let pair = (value % 2 == 0 ? value + 1 : value - 1);
+        this.vector[value] = [this.position[pair][0] - this.position[value][0], -this.position[pair][1] + this.position[value][1]];
+        this.vector[pair] = [-this.vector[value][0], -this.vector[value][1]];
+        this.distance[value] = Math.abs(this.vector[value][0]) + Math.abs(this.vector[value][1]);
+        this.distance[pair] = this.distance[value];
+        this.direction[value] = (this.vector[value][0] > 0 ? 2 : (this.vector[value][0] != 0) ? 5 : 1) * (this.vector[value][1] > 0 ? 7 : (this.vector[value][1] != 0) ? 3 : 1);
+        this.direction[pair] = (this.vector[pair][0] > 0 ? 2 : (this.vector[pair][0] != 0) ? 5 : 1) * (this.vector[pair][1] > 0 ? 7 : (this.vector[pair][1] != 0) ? 3 : 1);
+    }
+
+    matching(value) {
+        console.log(this.vector[value]);
+        if (this.distance != 1) {
+            let center = [...this.position[value]];
+            switch (this.direction) {
+                case 15:
+                    center[0] += 1;
+                case 35:
+
+                case 14:
+
+            }
+            if (this.direction % 3 == 0) {
+                center[0] += this.vector[value][1];
+            }
+            if (this.direction % 5 == 0) {
+                center[0] += this.vector[value][0];
+                center[1] -= this.vector[value][0];
+            }
+            if (this.direction % 7 == 0) {
+                center[1] += this.vector[value][1];
+            }
+            return { center: center, size: this.distance[value] };
+        }
+        return false;
     }
 }
 
--- procon36_server/process/main.js
+++ procon36_server/process/main.js
@@ -8,7 +8,11 @@ let procon=new Lampyrisma(4,5,5);
 procon.timer.start();
 console.log(procon.board);
 for(let i=0;i<16;i++){
-    console.log(procon.calcCenter(i));
+    console.log(procon.entities.matching(i));
 }
+console.log(procon.board);
+/*for(let i=0;i<100000;i++){
+    procon.engage(procon.board,[1,1],23);
+}*/
 procon.timer.end();
 procon.timer.show();
